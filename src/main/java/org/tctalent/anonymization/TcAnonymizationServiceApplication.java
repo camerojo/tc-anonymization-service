4e681e82d6de366d293142f8821d274cbdcd34da
John Cameron <john@cameronfoundation.org>
2024-11-01 14:03:41 +1100

    Initial commit

---
+++ src/main/java/org/tctalent/anonymization/TcAnonymizationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tctalent.anonymization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcAnonymizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcAnonymizationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tctalent/anonymization/TcAnonymizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tctalent.anonymization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcAnonymizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
