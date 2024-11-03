df23c2dc863f86bee5e82dea2f9b13f62cfd0249
john <jadcpub@gmail.com>
2024-11-03 17:06:14 +1100

    Add todos

--- src/main/java/org/tctalent/anonymization/TcAnonymizationServiceApplication.java
+++ src/main/java/org/tctalent/anonymization/TcAnonymizationServiceApplication.java
@@ -8,6 +8,9 @@ public class TcAnonymizationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TcAnonymizationServiceApplication.class, args);
+
+        //TODO JC Trigger login to server
+        //TODO JC Trigger query of all candidates
     }
 
 }
