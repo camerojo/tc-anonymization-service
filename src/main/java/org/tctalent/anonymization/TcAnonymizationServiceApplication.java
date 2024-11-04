328f6c1ffda1b0bf5a27f891f4ab9aff3d7a1fdf
john <jadcpub@gmail.com>
2024-11-04 11:27:03 +1100

    Can run this from command line

--- src/main/java/org/tctalent/anonymization/TcAnonymizationServiceApplication.java
+++ src/main/java/org/tctalent/anonymization/TcAnonymizationServiceApplication.java
@@ -1,16 +1,19 @@
 package org.tctalent.anonymization;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TcAnonymizationServiceApplication {
+public class TcAnonymizationServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TcAnonymizationServiceApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
         //TODO JC Trigger login to server (need app login - ie no MFA)
         //TODO JC Trigger query of all candidates
     }
-
 }
