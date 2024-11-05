3effd89e0010733bb8ab98cfd2c71665bab3d4f1
John Cameron <john@cameronfoundation.org>
2024-11-05 22:37:17 +1100

    Implement saveCandidates

--- src/main/java/org/tctalent/TcAnonymizationServiceApplication.java
+++ src/main/java/org/tctalent/TcAnonymizationServiceApplication.java
@@ -5,14 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.tctalent.server.repository.AnonCandidateRepository;
+import org.tctalent.anonymization.service.PopulateDatabaseService;
+import org.tctalent.anonymization.service.TalentCatalogService;
 
 @SpringBootApplication
 @EnableScheduling
 public class TcAnonymizationServiceApplication implements CommandLineRunner {
 
     @Autowired
-    AnonCandidateRepository anonCandidateRepository;
+    PopulateDatabaseService populateDatabaseService;
+
+    @Autowired
+    TalentCatalogService talentCatalogService;
 
     public static void main(String[] args) {
         SpringApplication.run(TcAnonymizationServiceApplication.class, args);
@@ -24,7 +28,9 @@ public class TcAnonymizationServiceApplication implements CommandLineRunner {
 //        System.out.println(s);
         //TODO JC Trigger login to server (need app login - ie no MFA)
         //TODO JC Trigger query of all candidates
+        talentCatalogService.login();
 
+        populateDatabaseService.populateDatabase(50);
 
     }
 }
