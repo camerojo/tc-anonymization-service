2fb3faacb5530ee5270a6b9e5192e2e43cf7c56a
sadatmalik <sadat.malik@me.com>
2024-11-18 13:24:15 +0000

    update basic unit test

--- src/test/java/org/tctalent/anonymization/service/AnonymizationServiceImplTest.java
+++ src/test/java/org/tctalent/anonymization/service/AnonymizationServiceImplTest.java
@@ -4,10 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.tctalent.anonymization.model.AnonCandidate;
+import org.tctalent.anonymization.config.JacksonConfig;
 import org.tctalent.anonymization.model.Candidate;
+import org.tctalent.anonymization.model.IdentifiableCandidate;
 
 /**
  * Test
@@ -19,16 +25,32 @@ class AnonymizationServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        service = new AnonymizationServiceImpl();
+        JacksonConfig jacksonConfig = new JacksonConfig();
+        ObjectMapper objectMapper = jacksonConfig.objectMapper();
+
+        service = new AnonymizationServiceImpl(objectMapper);
     }
 
     @Test
     void anonymize() throws JsonProcessingException {
-        Candidate candidate = new Candidate(12L, "Johnny", true);
 
-        AnonCandidate anonCandidate = service.anonymize(candidate);
+        UUID uuid = UUID.randomUUID();
 
-        assertEquals(12L, anonCandidate.getCandidateNumber());
-        assertTrue(anonCandidate.isMarried());
+        IdentifiableCandidate identifiableCandidate = IdentifiableCandidate.builder()
+            .id(uuid)
+            .name("John Doe")
+            .contact("+1-234-567-890")
+            .email("johndoe@example.com")
+            .address("123 Main St, Springfield, IL")
+            .dateOfBirth(LocalDate.parse("1985-06-15"))                           // Date of birth in yyyy-MM-dd format
+            .skills(List.of("Java", "Python", "AWS"))
+            .experience(5.5f)
+            .dateCreated(OffsetDateTime.now())
+            .build();
+
+        Candidate candidate = service.anonymize(identifiableCandidate);
+
+        assertEquals(uuid, candidate.getId());
+        System.out.println(candidate);
     }
 }
