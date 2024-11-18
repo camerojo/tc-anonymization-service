659e7659fae63d67f709aaae1bb77ea1703164b2
sadatmalik <sadat.malik@me.com>
2024-11-18 13:18:58 +0000

    jackson object mapper bean config

--- src/main/java/org/tctalent/anonymization/service/AnonymizationServiceImpl.java
+++ src/main/java/org/tctalent/anonymization/service/AnonymizationServiceImpl.java
@@ -1,11 +1,11 @@
 package org.tctalent.anonymization.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.tctalent.anonymization.model.AnonCandidate;
 import org.tctalent.anonymization.model.Candidate;
+import org.tctalent.anonymization.model.IdentifiableCandidate;
 
 /**
  * Copies fields from Candidate where there is a matching field in AnonCandidate
@@ -13,18 +13,18 @@ import org.tctalent.anonymization.model.Candidate;
  * @author John Cameron
  */
 @Service
+@RequiredArgsConstructor
 public class AnonymizationServiceImpl implements AnonymizationService{
-    private final ObjectMapper mapper = new ObjectMapper()
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private final ObjectMapper mapper;
 
     @Override
-    public AnonCandidate anonymize(Candidate candidate) throws JsonProcessingException {
+    public Candidate anonymize(IdentifiableCandidate candidate) throws JsonProcessingException {
         String json = mapper.writeValueAsString(candidate);
         return anonymize(json);
     }
 
     @Override
-    public AnonCandidate anonymize(String json) throws JsonProcessingException {
-        return mapper.readValue(json, AnonCandidate.class);
+    public Candidate anonymize(String json) throws JsonProcessingException {
+        return mapper.readValue(json, Candidate.class);
     }
 }
