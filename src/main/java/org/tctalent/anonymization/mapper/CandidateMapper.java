746e9de528efb6e1f62b1d92eaa5fa2c37d1d029
sadatmalik <sadat.malik@me.com>
2024-12-05 14:31:21 +0000

    map to mongo docs

--- src/main/java/org/tctalent/anonymization/mapper/CandidateMapper.java
+++ src/main/java/org/tctalent/anonymization/mapper/CandidateMapper.java
@@ -5,12 +5,19 @@ import org.mapstruct.Mapping;
 import org.springframework.data.domain.Page;
 import org.tctalent.anonymization.model.Candidate;
 import org.tctalent.anonymization.model.CandidatePage;
+import org.tctalent.anonymization.entity.mongo.CandidateDocument;
 
-@Mapper(uses = IdMapper.class)
+@Mapper(uses = {
+    IdMapper.class,
+    DateTimeMapper.class
+})
 public interface CandidateMapper {
 
   @Mapping(source = "id", target = "id")
   Candidate toModel(org.tctalent.anonymization.entity.db.Candidate entity);
 
   CandidatePage toCandidatePage(Page<Candidate> page);
+
+  @Mapping(source = "createdDate", target = "createdDate")
+  CandidateDocument toDocument(org.tctalent.anonymization.entity.db.Candidate entity);
 }
